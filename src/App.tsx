f8025e403aa70a1ec4297e05a113d1eb7fca6091
Veronika Golovanova <veronika.golovanova@vzp.cz>
2025-02-11 10:41:01 +0100

    clear project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
-import { debounce } from './utils/debounce';
 
 function App() {
-
-
-  const logMessage = (msg: string) => console.log(msg);
-
-  const debouncedLog = debounce(logMessage, 1000);
-  
-  debouncedLog("Привет!");
-  setTimeout(() => debouncedLog("Как дела?"), 500);
-  setTimeout(() => debouncedLog("Прошла 1 секунда"), 1500);
-
-
 
 
   return (
